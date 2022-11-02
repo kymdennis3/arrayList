cc55f9dfb901b95b58fa5a6908ecc224eea055e5
kymdennis3 <kymdennis3@gmail.com>
2022-11-02 08:09:19 +0300

    used function .set() to replace an element

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Arraylist = a resizable array.
+                      Elements can be added and removed after compilation phase
+                      store reference data types
+         */
+
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("Chicken");
+        food.add("Pizza");
+        food.add("Hamburger");
+
+        food.set(0,"Ugali");
+
+        for(int i = 0; i< food.size(); i++){
+        System.out.println(food.get(i));
     }
-}
+}}
