f159172740750d7a4c8456b8b13640822a12131c
kymdennis3 <kymdennis3@gmail.com>
2022-11-23 19:08:04 +0300

    used function .clear() to clear the ArrayList

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,11 @@ public class Main {
                       store reference data types
          */
 
-        ArrayList<String> food = new ArrayList<String>();
+        ArrayList<String> food = new ArrayList();
 
         food.add("Chicken");
         food.add("Pizza");
         food.add("Hamburger");
-
-        food.set(0,"Ugali");
-        food.remove(2);
-        food.clear();
 
         for(int i = 0; i< food.size(); i++){
         System.out.println(food.get(i));
